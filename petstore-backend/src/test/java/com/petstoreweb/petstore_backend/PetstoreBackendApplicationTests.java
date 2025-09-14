459a010ca56428ffd7a3802c478a4e80df2aa157
alejorendons <diegoo.alejo74@gmail.com>
2025-09-14 16:09:31 -0500

    Initial commit of petstore-backend project

---
+++ petstore-backend/src/test/java/com/petstoreweb/petstore_backend/PetstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petstoreweb.petstore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
